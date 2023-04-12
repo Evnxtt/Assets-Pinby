97601823d59f1f9ee17a60fea2636826c581ead9
Venext <119793154+Evnxtt@users.noreply.github.com>
2023-04-12 21:09:04 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysqli_connect("localhost","root","","API");
+$response = array();
+if($con){
+    $sql = "select * from users";
+    $result = mysqli_query($con,$sql);
+    if($result){
+        header("Content-Type: JSON");
+        $i=0;
+        while($row = mysqli_fetch_assoc($result)){
+            $response[$i]['id'] = $row ['id'];
+            $response[$i]['name'] = $row ['name'];
+            $response[$i]['age'] = $row ['age'];
+            $i++;
+        }
+        echo json_encode($response,JSON_PRETTY_PRINT);
+    }
+}else{
+    echo "Failed";
+}
+?>
